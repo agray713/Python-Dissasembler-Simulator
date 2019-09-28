a89462abec841e8e51ce086d0c1885c9c9a0b6bc
agray713 <38847795+agray713@users.noreply.github.com>
2019-09-27 19:19:13 -0500

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,4 +1,6 @@
 import sys
-print("this is the name of the script",sys.argv[0])
-print("Number of arguments: ", len(sys.argv))
-print("The arguments are: " , str(sys.argv))
+from helper import SetUp
+import disassembler
+import os
+
+SetUp.get_input_filename()
